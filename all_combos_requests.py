bd4e5fc1e7e4aa935102c39b36e0de8005514825
Pixelz22 <tljohnson1225@gmail.com>
2024-02-18 16:20:52 -0500

    edited combo generation order
    now the program finishes the last batch before moving on.
    
    Should help keep track of the earliest level certain recipes appear at

--- all_combos_requests.py
+++ all_combos_requests.py
@@ -8,7 +8,6 @@ import time
 
 HISTORY: dict[str, int | list[str] | list[int]] = {}
 RECIPES: dict[str, list[str]] = {}
-LEVELS: dict[str, int] = {}
 NULL_RECIPE_KEY = "%NULL%"
 
 SESSION: requests.sessions.Session | None = None
@@ -38,26 +37,33 @@ def combine(one: str, two: str):
     return json.loads(response.content.decode('utf-8'))
 
 
-def all_combos(max_idx: int, min_idx=0, ) -> list[tuple[int, int]]:
+def all_combos() -> list[tuple[int, int]]:
     """
     Generates all combination of numbers below max_idx.
     Ignores all combinations in which both numbers are below min_idx.
     """
     combos = []
+    batch_size = len(HISTORY["elements"])
 
-    # Start right where we last left off.
-    for j in range(HISTORY["last_combo"][1], max_idx):
-        combos.append((HISTORY["last_combo"][0], j))
+    # Finish last batch first
+    for j in range(HISTORY["last_combo"][1], HISTORY["last_batch_size"]):
+        combos.append((HISTORY["last_combo"][0], j))  # Only check ones that came after our last combo
 
-    for i in range(HISTORY["last_combo"][0] + 1, max_idx):
-        for j in range(max(i, min_idx), max_idx):
+    for i in range(HISTORY["last_combo"][0] + 1, HISTORY["last_batch_size"]):
+        for j in range(i, HISTORY["last_batch_size"]):
+            combos.append((i, j))  # Only check ones that came after our last combo
+
+    # Now start on the next level
+    for i in range(0, batch_size):
+        for j in range(max(i, HISTORY["last_batch_size"]), batch_size):
             combos.append((i, j))
     return combos
 
 
-def produce_all_combinations(level: int, ignore_below=0, sleep=0.0) -> int:
+def produce_all_combinations(sleep=0.0) -> int:
     batch_size = len(HISTORY["elements"])
-    combos = all_combos(batch_size, min_idx=ignore_below)
+    combos = all_combos()
+    HISTORY["last_batch_size"] = batch_size
 
     for combo in combos:
         e1 = HISTORY["elements"][combo[0]]
@@ -83,10 +89,6 @@ def produce_all_combinations(level: int, ignore_below=0, sleep=0.0) -> int:
         # Update our history
         if result_key not in HISTORY["elements"]:
             HISTORY["elements"].append(result_key)
-
-        # Update Levels
-        if result_json["result"] not in LEVELS:
-            LEVELS[result_json["result"]] = level
 
         # Keep track of new discoveries
         if result_json["isNew"]:
@@ -116,29 +118,17 @@ if __name__ == "__main__":
         with open("levels.json", "r") as fp:
             LEVELS = json.load(fp)
 
-    last_batch_size = HISTORY["last_batch_size"]  # Keeps track of the highest index we haven't checked
-    level = HISTORY["last_level"]  # Keeps track of our current level
     try:
-        # Main Loop
         SESSION = requests.session()
         SESSION.headers = rHEADERS
 
-        c = input("Continue? ")
-        if c == "N" or c == "n":
-            sys.exit(0)
-
         start = time.time_ns()
-        last_batch_size = produce_all_combinations(level + 1, ignore_below=last_batch_size, sleep=0.15)
+        produce_all_combinations(sleep=0.15)
         duration = (time.time_ns() - start) / 1000000000
-        level += 1  # Don't increase the level counter until we've completed the level
-        print(f"Level {level}: Duration - {duration} s;")
+        print(f"Duration - {duration} s;")
 
     finally:
-        HISTORY["last_level"] = level
-        HISTORY["last_batch_size"] = last_batch_size
         with open("history.json", "w") as fp:
             json.dump(HISTORY, fp, indent=4)
         with open("recipes.json", "w") as fp:
             json.dump(RECIPES, fp, indent=4)
-        with open("levels.json", "w") as fp:
-            json.dump(LEVELS, fp, indent=4)
