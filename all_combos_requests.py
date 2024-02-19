797fbf1e4ea27405fd8f88353b180b16b2a6e22f
Pixelz22 <tljohnson1225@gmail.com>
2024-02-19 08:50:28 -0500

    Final form (sorta)
    - Program now waits to create next batch until last batch is finished (ensures that the order of discovery is consistent with gameplay progress)
    - is now keeping track of element levels again
    - can run for ever, will automatically stop itself if something goes wrong and pick up where it left off
    - saves data after each level

--- all_combos_requests.py
+++ all_combos_requests.py
@@ -5,8 +5,10 @@ import json
 import os.path
 import time
 
+from utilities import DelayedKeyboardInterrupt
+
 HISTORY_FILE = "history.json"
-HISTORY: dict[str, int | list[str] | list[int]] = {}
+HISTORY: dict[str, int | list[any] | dict[str, int]] = {}
 RECIPE_FILE = "recipes-1.json"
 RECIPES: dict[str, list[str]] = {}
 NULL_RECIPE_KEY = "%NULL%"
@@ -38,35 +40,8 @@ def combine(one: str, two: str):
     return json.loads(response.content.decode('utf-8'))
 
 
-def all_combos() -> list[tuple[int, int]]:
-    """
-    Generates all combination of numbers below max_idx.
-    Ignores all combinations in which both numbers are below min_idx.
-    """
-    combos = []
-    batch_size = len(HISTORY["elements"])
-
-    # Finish last batch first
-    for j in range(HISTORY["last_combo"][1], HISTORY["last_batch_size"]):
-        combos.append((HISTORY["last_combo"][0], j))  # Only check ones that came after our last combo
-
-    for i in range(HISTORY["last_combo"][0] + 1, HISTORY["last_batch_size"]):
-        for j in range(i, HISTORY["last_batch_size"]):
-            combos.append((i, j))  # Only check ones that came after our last combo
-
-    # Now start on the next level
-    for i in range(0, batch_size):
-        for j in range(max(i, HISTORY["last_batch_size"]), batch_size):
-            combos.append((i, j))
-    return combos
-
-
-def produce_all_combinations(sleep=0.0) -> int:
-    batch_size = len(HISTORY["elements"])
-    combos = all_combos()
-    HISTORY["last_batch_size"] = batch_size
-
-    for combo in combos:
+def process_partial_batch(batch: list[tuple[int, int]], sleep=0.0):
+    for combo in batch:
         e1 = HISTORY["elements"][combo[0]]
         e2 = HISTORY["elements"][combo[1]]
         recipe_key = e1 + ";" + e2
@@ -91,6 +66,9 @@ def produce_all_combinations(sleep=0.0) -> int:
         if result_key not in HISTORY["elements"]:
             HISTORY["elements"].append(result_key)
 
+        if result_key not in HISTORY["levels"]:
+            HISTORY["levels"][result_key] = HISTORY["level"]
+
         # Keep track of new discoveries
         if result_json["isNew"]:
             print(f"NEW DISCOVERY: {e1} + {e2} = {result_key}")
@@ -100,7 +78,24 @@ def produce_all_combinations(sleep=0.0) -> int:
         HISTORY["last_combo"] = list(combo)
         time.sleep(sleep)
 
-    return batch_size
+
+def progress(sleep=0.0):
+    last_batch_partial = []
+
+    # Process patch
+    for j in range(HISTORY["last_combo"][1], HISTORY["batch_size"]):
+        last_batch_partial.append((HISTORY["last_combo"][0], j))  # Only check ones that came after our last combo
+
+    for i in range(HISTORY["last_combo"][0] + 1, HISTORY["batch_size"]):
+        for j in range(max(i, HISTORY["last_batch_size"]), HISTORY["batch_size"]):
+            last_batch_partial.append((i, j))  # Only check ones that came after our last combo
+
+    process_partial_batch(last_batch_partial, sleep=sleep)
+
+    with DelayedKeyboardInterrupt():  # Ensures we don't accidentally exit the code while updating crucial data
+        HISTORY["last_batch_size"] = HISTORY["batch_size"]
+        HISTORY["batch_size"] = len(HISTORY["elements"])
+        HISTORY["level"] += 1
 
 
 if __name__ == "__main__":
@@ -120,10 +115,19 @@ if __name__ == "__main__":
         SESSION = requests.session()
         SESSION.headers = rHEADERS
 
-        start = time.time_ns()
-        produce_all_combinations(sleep=0.15)
-        duration = (time.time_ns() - start) / 1000000000
-        print(f"Duration - {duration} s;")
+        while True:
+            start = time.time_ns()
+            progress(sleep=0.125)
+            duration = (time.time_ns() - start) / 1000000000
+            print(f"LEVEL {HISTORY['level'] - 1}: Duration - {duration} s; "
+                  f"Found {HISTORY['batch_size'] - HISTORY['last_batch_size']} new crafts")
+            print("---------------")
+
+            # Save data after each level
+            with open(HISTORY_FILE, "w") as fp:
+                json.dump(HISTORY, fp, indent=4)
+            with open(RECIPE_FILE, "w") as fp:
+                json.dump(RECIPES, fp, indent=4)
 
     finally:
         SESSION.close()
